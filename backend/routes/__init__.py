a6ea5e3e6d7e6f089e014c4ddca9a18819277fd9
shehroz <165585892+shehrozmajeed@users.noreply.github.com>
2026-04-19 19:04:18 +0500

    Update __init__.py

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -4,5 +4,6 @@
 from .student    import student_bp
 from .alumni     import alumni_bp
 from .admin      import admin_bp
+from .mentorship import mentorship_bp
 
-__all__ = ["student_bp", "alumni_bp", "admin_bp"]
+__all__ = ["student_bp", "alumni_bp", "admin_bp", "mentorship_bp"]
